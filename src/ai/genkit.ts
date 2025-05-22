1f3cd84c4c4140007482eb08c7ce8b45b3b366c0
sheoranjatin9 <sheoranjatin9@gmail.com>
2025-05-22 14:13:42 +0000

    changes 1 - firebase

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,17 +1,9 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {replicate} from '@genkit-ai/replicate';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    replicate({
-      sketchToRenderApiKey: process.env.SKETCH_TO_RENDER_REPLICATE_API_KEY,
-      moodboardRenderApiKey: process.env.MOODBOARD_RENDER_REPLICATE_API_KEY,
-      textToRenderApiKey: process.env.TEXT_TO_RENDER_REPLICATE_API_KEY,
-      enhanceDetailsApiKey: process.env.ENHANCE_DETAILS_REPLICATE_API_KEY,
-      styleSuggestionApiKey: process.env.STYLE_SUGGESTION_REPLICATE_API_KEY,
-    }),
   ],
   model: 'googleai/gemini-2.0-flash',
 });
